cac4c6048dcafc34da3698e45c309aa0020b4e8f
avmochalov <al.mochalov93@gmail.com>
2023-06-05 16:57:17 +0300

    Картинка рубашки

--- components/first-level.js
+++ components/first-level.js
@@ -1,9 +1,13 @@
+import { headerRenderer } from './header.js';
+
 export function firstLevelPageRenderer({ app }) {
-    const appHtml = `<div class="level_choice__wrapper">
+    const appHtml = `<div class="header_component"></div>
+    <div class="level_choice__wrapper">
     <div class="level_choice">
         <p class="level_choice__text">Уровень 1</p>
     </div>
 </div>`;
 
     app.innerHTML = appHtml;
+    headerRenderer({ element: document.querySelector('.header_component') });
 }

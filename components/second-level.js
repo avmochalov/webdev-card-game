99e1415e301097e2e9ac4d10bfe2fabfd6c82c5f
avmochalov <al.mochalov93@gmail.com>
2023-06-05 20:54:04 +0300

    Реализация остальных уровней

--- components/second-level.js
+++ components/second-level.js
@@ -1,9 +1,23 @@
+import { cardsTableGenerate } from './cards-table.js';
+import { headerRenderer } from './header.js';
 export function secondLevelPageRenderer({ app }) {
-    const appHtml = `<div class="level_choice__wrapper">
-    <div class="level_choice">
-        <p class="level_choice__text">Уровень 2</p>
-    </div>
-</div>`;
-
+    const appHtml = `<div class="header_component"></div>
+    <div class="card_field"> </div>
+`;
+    let backSideCards = [];
+    for (let i = 0; i < 12; i++) {
+        backSideCards.push(
+            '<img src="./pic/backside.svg" alt="card-backside">'
+        );
+    }
+    console.log(backSideCards);
     app.innerHTML = appHtml;
+    document.querySelector('.card_field').innerHTML = backSideCards.join('');
+    setTimeout(
+        () =>
+            (document.querySelector('.card_field').innerHTML =
+                cardsTableGenerate(12)),
+        5000
+    );
+    headerRenderer({ element: document.querySelector('.header_component') });
 }

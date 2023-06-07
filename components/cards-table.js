f10804caa1df45a139dc3926104fe4aa8af4bd96
avmochalov <al.mochalov93@gmail.com>
2023-06-07 16:57:37 +0300

    Реализован выбор пар карт

--- components/cards-table.js
+++ components/cards-table.js
@@ -10,33 +10,31 @@ export function cardsTableGenerate(value) {
         .concat(randomCards)
         .sort(() => 0.5 - Math.random());
     console.log(gameCardSet);
-    const cardsTable = gameCardSet
-        .map((card) => {
-            let suit;
-            switch (card.charAt(0)) {
-                case 'П':
-                    suit = './pic/pik.svg';
-                    break;
-                case 'Ч':
-                    suit = './pic/che.svg';
-                    break;
-                case 'Б':
-                    suit = './pic/bub.svg';
-                    break;
-                case 'К':
-                    suit = './pic/krest.svg';
-                    break;
-                default:
-                    break;
-            }
-            return `<div class="card">
+    const cardsTable = gameCardSet.map((card) => {
+        let suit;
+        switch (card.charAt(0)) {
+            case 'П':
+                suit = './pic/pik.svg';
+                break;
+            case 'Ч':
+                suit = './pic/che.svg';
+                break;
+            case 'Б':
+                suit = './pic/bub.svg';
+                break;
+            case 'К':
+                suit = './pic/krest.svg';
+                break;
+            default:
+                break;
+        }
+        return `<div class="card" data-cardValue='${card}'>
             <p class="card_value__up">${card.slice(1)}</p>
             <p class="card_value__down">${card.slice(1)}</p>
             <img src="${suit}" alt="pik" class="suit_big">
             <img src="${suit}" alt="pik" class="suit_small__up">
             <img src="${suit}" alt="pik" class="suit_small__down">
         </div>`;
-        })
-        .join('');
+    });
     return cardsTable;
 }
--- components/level-render.js
+++ components/level-render.js
@@ -7,17 +7,36 @@ export function levelPageRenderer({ app }) {
     let backSideCards = [];
     for (let i = 0; i < 6 * Number(localStorage.getItem('Level')); i++) {
         backSideCards.push(
-            '<img src="./pic/backside.svg" alt="card-backside">'
+            `<div class="card" data-id='${i}'> <img src="./pic/backside.svg" alt="card-backside"> </div>`
         );
     }
     console.log(backSideCards);
     app.innerHTML = appHtml;
-    document.querySelector('.card_field').innerHTML = backSideCards.join('');
-    setTimeout(
-        () =>
-            (document.querySelector('.card_field').innerHTML =
-                cardsTableGenerate(backSideCards.length)),
-        2000
-    );
+    const cardTable = cardsTableGenerate(backSideCards.length);
+    document.querySelector('.card_field').innerHTML = cardTable.join('');
+
+    setTimeout(() => {
+        document.querySelector('.card_field').innerHTML =
+            backSideCards.join('');
+        let clickedCard = [];
+        for (const card of document.querySelectorAll('.card')) {
+            card.addEventListener('click', (event) => {
+                event.stopPropagation();
+                clickedCard.push(cardTable[card.dataset.id]);
+                let targetCard = document.createElement('div');
+                targetCard.innerHTML = cardTable[card.dataset.id];
+                card.replaceWith(targetCard);
+                if (clickedCard.length === 2) {
+                    if (clickedCard[0] === clickedCard[1]) {
+                        alert('Вы победили!');
+                        clickedCard = [];
+                    } else {
+                        alert('Вы проиграли!');
+                        clickedCard = [];
+                    }
+                }
+            });
+        }
+    }, 2000);
     headerRenderer({ element: document.querySelector('.header_component') });
 }
